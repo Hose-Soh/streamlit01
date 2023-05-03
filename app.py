d44109b0cae45437643fc788f7c787615d75ea8a
MD Hossen Sohrawardy <113468872+Hose-Soh@users.noreply.github.com>
2023-05-03 20:11:33 +0600

    testing

--- app.py
+++ app.py
@@ -431,9 +431,9 @@ pprint.pprint(local_pr)
 
 
 
-# def ee_array_to_df(arr, list_of_bands):
-#     """Transforms client-side ee.Image.getRegion array to pandas.DataFrame."""
-#     df = pd.DataFrame(arr)
+def ee_array_to_df(arr, list_of_bands):
+    """Transforms client-side ee.Image.getRegion array to pandas.DataFrame."""
+    df = pd.DataFrame(arr)
 
 #     # Rearrange the header.
 #     headers = df.iloc[0]
