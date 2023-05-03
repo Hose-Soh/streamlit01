14b642cecaf670e51e2ad841c98d9e787d158f1e
MD Hossen Sohrawardy <113468872+Hose-Soh@users.noreply.github.com>
2023-05-03 20:12:19 +0600

    testing

--- app.py
+++ app.py
@@ -435,24 +435,24 @@ def ee_array_to_df(arr, list_of_bands):
     """Transforms client-side ee.Image.getRegion array to pandas.DataFrame."""
     df = pd.DataFrame(arr)
 
-#     # Rearrange the header.
-#     headers = df.iloc[0]
-#     df = pd.DataFrame(df.values[1:], columns=headers)
+    # Rearrange the header.
+    headers = df.iloc[0]
+    df = pd.DataFrame(df.values[1:], columns=headers)
 
-#     # Convert the data to numeric values.
-#     for band in list_of_bands:
-#         df[band] = pd.to_numeric(df[band], errors="coerce")
+    # Convert the data to numeric values.
+    for band in list_of_bands:
+        df[band] = pd.to_numeric(df[band], errors="coerce")
 
-#     # Convert the time field into a datetime.
-#     df["datetime"] = pd.to_datetime(df["time"], unit="ms")
+    # Convert the time field into a datetime.
+    df["datetime"] = pd.to_datetime(df["time"], unit="ms")
 
-#     # Keep the columns of interest.
-#     df = df[["time", "datetime", *list_of_bands]]
+    # Keep the columns of interest.
+    df = df[["time", "datetime", *list_of_bands]]
 
-#     # The datetime column is defined as index.
-#     df = df.set_index("datetime")
+    # The datetime column is defined as index.
+    df = df.set_index("datetime")
 
-#     return df
+    return df
 
 # pr_df = ee_array_to_df(local_pr, ["precipitation"])
 # #pr_df.head(10)
