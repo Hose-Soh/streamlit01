e94a0c71ae0b821205d0c985d357d3d94c7a1369
MD Hossen Sohrawardy <113468872+Hose-Soh@users.noreply.github.com>
2023-05-03 17:02:22 +0600

    testing

--- app.py
+++ app.py
@@ -275,7 +275,11 @@ profile_orgm = local_profile(orgm, poi, scale)
 
 #Print the organic matter content profile.
 st.write("Organic Matter content profile at the location of interest:")
-st.write(profile_orgm.getInfo())
+try:
+    st.write(profile_orgm.getInfo())
+except IndexError:
+    st.write("Error: profile_orgm is empty or index is out of range.")
+
 
 #Initialization of two constant images for wilting point and field capacity.
 wilting_point = ee.Image(0)
